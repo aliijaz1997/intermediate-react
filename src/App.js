85e500a440927bc272c4ddbfb11dedb9b9e3f8ce
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-05-17 17:49:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
 
+const initialState = { count: 0 };
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+
+    case "decrement":
+      return { count: state.count - 1 };
+
+    case "reset":
+      return { count: 0 };
+
+    default:
+      return state;
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="text-warning">
+        Current value of the counter is ={" "}
+        <span className="text-dark">{state.count}</span>
+      </h3>
+      <button
+        className="btn btn-primary m-1"
+        onClick={() => {
+          dispatch({ type: "increment" });
+        }}
+      >
+        +
+      </button>
+      <button
+        className="btn btn-primary m-1"
+        onClick={() => {
+          dispatch({ type: "decrement" });
+        }}
+      >
+        -
+      </button>
+      <br />
+      <button
+        className="btn btn-danger m-1"
+        onClick={() => {
+          dispatch({ type: "reset" });
+        }}
+      >
+        Reset Counter
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { reducer } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("`increment` should increase the count", () => {
+  const initialState = { count: 0 };
+  const newState = reducer(initialState, { type: "increment" });
+  expect(newState.count).toBe(1);
+});
+test("`decrement` should decrease the count", () => {
+  const initialState = { count: 1 };
+  const newState = reducer(initialState, { type: "decrement" });
+  expect(newState.count).toBe(0);
+});
+test("`reset` should reset the count", () => {
+  const initialState = { count: 1 };
+  const newState = reducer(initialState, { type: "reset" });
+  expect(newState.count).toBe(0);
 });
